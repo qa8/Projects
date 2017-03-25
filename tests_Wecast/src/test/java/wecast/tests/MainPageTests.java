6173481332de5ae130c25343fac62c248160d2a7
Smip111 <smip@inbox.ru>
2017-03-25 16:53:19 +0300

    tests_Wekast_Maven created
    A new project for Wekast tests with using Maven

--- tests_Wecast/src/test/java/wecast/tests/MainPageTests.java
+++ tests_Wecast/src/test/java/wecast/tests/MainPageTests.java
@@ -9,61 +9,61 @@ import static org.testng.Assert.assertTrue;
  */
 public class MainPageTests extends TestBase {
 
-    @Test
+    @Test(groups = "smoke")
     public void testOpenMainPage(){
           assertTrue(app.goTo().isElementPresent(By.linkText("LEARN MORE")));
     }
 
-    @Test
+    @Test(groups = "regression")
     public void testLearnMore(){
         app.goTo().learnMore();
         assertTrue(app.getDriver().getCurrentUrl().contains("indiegogo"));
         app.getDriver().navigate().back();
     }
 
-    @Test
+    @Test(groups = "regression")
     public void testMenuStory(){
         app.goTo().storyPage();
         assertTrue(app.goTo().isElementPresent(By.id("post-1797")));
         app.getDriver().navigate().back();
     }
 
-    @Test
+    @Test(groups = "regression")
     public void testMenuTechnology(){
         app.goTo().technologyPage();
         assertTrue(app.goTo().isElementPresent(By.linkText("DOWNLOAD OUR FREE APP")));
         app.getDriver().navigate().back();
     }
 
-    @Test
+    @Test(groups = "regression")
     public void testMenuFaqs(){
         app.goTo().faqsPage();
         assertTrue(app.goTo().isElementPresent(By.linkText("setup instructions")));
         app.getDriver().navigate().back();
     }
 
-    @Test
+    @Test(groups = "regression")
     public void testMenuBlog(){
         app.goTo().blogPage();
         assertTrue(app.goTo().isElementPresent(By.linkText("PRESENTATION TIPS")));
         app.getDriver().navigate().back();
     }
 
-    @Test
+    @Test(groups = "regression")
     public void testMenuContact(){
         app.goTo().contactPage();
         assertTrue(app.goTo().isElementPresent(By.linkText("info@wekast.com")));
         app.getDriver().navigate().back();
     }
 
-    @Test
+    @Test(groups = "regression")
     public void testMenuPreOrder(){
         app.goTo().preOrderPage();
         assertTrue(app.goTo().isElementPresent(By.linkText("Click here to –>Pre-Order")));
         app.getDriver().navigate().back();
     }
 
-    @Test
+    @Test(groups = "smoke")
     public void testDownloadAppPage(){
         app.goTo().downloadAppPage();
         assertTrue(app.getDriver().getCurrentUrl().contains("google"));
